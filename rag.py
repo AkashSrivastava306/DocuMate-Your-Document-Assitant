85b2838a02c05d28633ca993852ad12c7b541f1c
Akash Srivastava <60046522+AkashSrivastava306@users.noreply.github.com>
2026-04-29 02:40:10 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -14,7 +14,7 @@ GROQ_API_KEY = st.secrets.get("GROQ_API_KEY")
 
 #llm
 llm = ChatGroq(
-    model="llama3-8b-8192",
+    model="llama3-70b-8192",
     temperature=0,
     groq_api_key=GROQ_API_KEY
 )
